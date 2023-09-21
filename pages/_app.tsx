c660f3c177258833f7aca1ba102d3d3f42090444
Chunghee Hwang <hch0821@gmail.com>
2023-09-21 15:33:09 +0900

    chore: react-query 설정 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,13 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app';
-
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
