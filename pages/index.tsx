cdeced716c8c36c94c00e651d8bd8fa78203bfc0
Chunghee Hwang <hch0821@gmail.com>
2023-09-22 23:41:26 +0900

    fix: 메인페이지 마운트 전에 router 사용 시 에러 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Pages from '@/constants/pages';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Index() {
   const router = useRouter();
-  router.replace(Pages.CHECK_USER_DATA_PAGE.URL);
+  useEffect(() => {
+    router.replace(Pages.CHECK_USER_DATA_PAGE.URL);
+  }, []);
+
   return <></>;
 }
