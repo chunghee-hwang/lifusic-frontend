119aba6094b4fd18467e1bf726a6556fac64cf5a
Chunghee Hwang <hch0821@gmail.com>
2023-09-25 03:34:05 +0900

    refactor: 페이지에 분리된 컴포넌트 적용

--- pages/management/musics.tsx
+++ pages/management/musics.tsx
@@ -1,20 +1,6 @@
 import useCheckRole from '@/hooks/check-role';
 import { Container } from '@/styles/global-style';
-import {
-  Box,
-  IconButton,
-  Paper,
-  Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  TableSortLabel,
-} from '@mui/material';
-import Checkbox from '@mui/material/Checkbox';
+import { Alert, CircularProgress, IconButton, Stack } from '@mui/material';
 import {
   OrderDirection,
   SearchArtistMusicRequest,
@@ -25,6 +11,7 @@ import SearchBar from '@/components/SearchBar';
 import { GET_DOWNLOAD_MUSIC_URL } from '@/apis/admin-apis';
 import { Delete, Download } from '@mui/icons-material';
 import useDeleteMusicMutation from '@/hooks/admin-api-hooks/delete-music-mutation';
+import SortableTable from '@/components/SortableTable';
 export default function MusicManagement() {
   useCheckRole();
   const [searchRequest, setSearchRequest] = useState<SearchArtistMusicRequest>(
@@ -57,34 +44,7 @@ export default function MusicManagement() {
     setSearchRequest(req);
   }, [keyword, page, orderDirection, limit]);
 
-  const toggleOrderDirection = useCallback(() => {
-    setOrderDirection(orderDirection === 'asc' ? 'desc' : 'asc');
-    setSelected(new Set());
-  }, [orderDirection]);
-
   const [selected, setSelected] = useState<Set<number>>(new Set<number>());
-  const isSelected = useCallback((id: number) => selected.has(id), [selected]);
-
-  const handleSelectAllRow = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      setSelected(new Set(rows?.map((row) => row.id) || []));
-      return;
-    }
-    setSelected(new Set());
-  };
-
-  const handleRowClick = useCallback(
-    (event: React.MouseEvent<unknown>, rowId: number) => {
-      const newSelected = new Set(selected);
-      if (newSelected.has(rowId)) {
-        newSelected.delete(rowId);
-      } else {
-        newSelected.add(rowId);
-      }
-      setSelected(newSelected);
-    },
-    [selected]
-  );
 
   const onClickDelete = useCallback(() => {
     if (selected.size < 1) {
@@ -138,145 +98,93 @@ export default function MusicManagement() {
   );
 
   const rows = useMemo(() => {
-    return data?.musics?.map((music) => {
-      const thumbnailImage = music.thumbnailImageUrl ? (
-        <img src={music.thumbnailImageUrl} width="50px" height="50px" />
-      ) : null;
-      const downloadLink = (
-        <IconButton
-          href={GET_DOWNLOAD_MUSIC_URL(music.id)}
-          target={'_blank'}
-          size="small"
-        >
-          <Download />
-        </IconButton>
-      );
-      return {
-        id: music.id,
-        columns: [
-          {
-            content: thumbnailImage,
-            style: {
-              width: 'auto',
-              height: '3.125rem',
+    return (
+      data?.musics?.map((music) => {
+        const thumbnailImage = music.thumbnailImageUrl ? (
+          <img src={music.thumbnailImageUrl} width="50px" height="50px" />
+        ) : null;
+        const downloadLink = (
+          <IconButton
+            href={GET_DOWNLOAD_MUSIC_URL(music.id)}
+            target={'_blank'}
+            size="small"
+          >
+            <Download />
+          </IconButton>
+        );
+        return {
+          id: music.id,
+          columns: [
+            {
+              id: 'thumbnailImage',
+              content: thumbnailImage,
+              style: {
+                width: 'auto',
+                height: '3.125rem',
+              },
             },
-          },
-          {
-            content: music.name,
-            style: {
-              width: '25rem',
+            {
+              id: 'musicName',
+              content: music.name,
+              style: {
+                width: '25rem',
+              },
             },
-          },
-          {
-            content: downloadLink,
-            style: {},
-          },
-        ],
-      };
-    });
+            {
+              id: 'downloadLink',
+              content: downloadLink,
+            },
+          ],
+        };
+      }) || []
+    );
   }, [data]);
 
   const totalRowsCount = data?.allMusicSize ?? 0;
 
-  const isAllSelected = useMemo(
-    () => (selected.size > 0 && selected.size === rows?.length) ?? 0,
-    [selected, rows]
-  );
-
   return (
     <Container>
       <Stack direction={'row'}>
-        <SearchBar setKeyword={setKeyword} placeholder="곡명으로 검색" />
+        <SearchBar
+          setKeyword={(keyword) => {
+            setPage(1);
+            setKeyword(keyword);
+          }}
+          placeholder="곡명으로 검색"
+        />
       </Stack>
-
-      <Box sx={{ width: 750 }} marginTop={2}>
-        <Paper sx={{ width: '100%' }}>
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      color="primary"
-                      onChange={handleSelectAllRow}
-                      checked={isAllSelected}
-                    />
-                  </TableCell>
-                  {headCells.map((headCell) => {
-                    const isSorted =
-                      headCell.sortable && orderBy === headCell.id;
-                    return (
-                      <TableCell
-                        key={headCell.id}
-                        sortDirection={isSorted ? orderDirection : false}
-                        align="justify"
-                        padding="none"
-                      >
-                        {(headCell.sortable && (
-                          <TableSortLabel
-                            active={isSorted}
-                            direction={isSorted ? orderDirection : 'asc'}
-                            onClick={() => {
-                              setOrderBy(headCell.id);
-                              toggleOrderDirection();
-                            }}
-                          >
-                            {headCell.content}
-                          </TableSortLabel>
-                        )) ||
-                          headCell.content}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows?.map((row) => {
-                  const isItemSelected = isSelected(row.id);
-                  return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.id}
-                      selected={isItemSelected}
-                      sx={{ cursor: 'pointer' }}
-                      onClick={(event) => handleRowClick(event, row.id)}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox color="primary" checked={isItemSelected} />
-                      </TableCell>
-                      {row.columns.map((column) => {
-                        return (
-                          <TableCell padding="none" style={column.style}>
-                            {column.content}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25, 50, 100]}
-            component="div"
-            count={totalRowsCount}
-            rowsPerPage={limit}
-            page={page - 1}
-            onPageChange={(event: unknown, newPage: number) => {
-              setPage(newPage + 1);
-              setSelected(new Set());
-            }}
-            onRowsPerPageChange={(event) => {
-              setPage(1);
-              setLimit(+event.target.value);
-            }}
-          />
-        </Paper>
-      </Box>
+      {totalRowsCount > 0 && (
+        <SortableTable
+          limit={limit}
+          setLimit={setLimit}
+          page={page}
+          setPage={setPage}
+          orderBy={orderBy}
+          setOrderBy={setOrderBy}
+          orderDirection={orderDirection}
+          setOrderDirection={setOrderDirection}
+          selected={selected}
+          setSelected={setSelected}
+          headCells={headCells}
+          rows={rows}
+          totalRowsCount={totalRowsCount}
+        />
+      )}
+      {isSearching && (
+        <Stack marginTop={10}>
+          <CircularProgress color="success" />
+        </Stack>
+      )}
+      {!isSearchError && !isSearching && totalRowsCount < 1 && (
+        <Stack marginTop={10}>검색 결과가 없습니다.</Stack>
+      )}
+      {isSearchError && (
+        <Stack marginTop={10}>
+          <Alert severity="error">
+            검색에 실패하였습니다. 다시 시도해주세요.
+          </Alert>
+        </Stack>
+      )}
     </Container>
   );
 }
